d337ea120c0e00a476569ff7a856eb3a0b6865de
Leonardo Pulicati <150066475+puliat@users.noreply.github.com>
2024-12-03 17:18:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if st.sidebar.button("Get Details"):
     ]
 
     if not location_data.empty:
-        col1, col2 = st.columns([1.5, 2])  # Adjust the ratio as needed
+        col1, col2 = st.columns([0.5, 1])  # Adjust the ratio as needed
 
         with col1:
             # Points of Interest Section
